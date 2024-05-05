115c8debe4d26b679b7dfb31447d4d92ba504c74
M-Wyeth <156701435+M-Wyeth@users.noreply.github.com>
2024-05-05 04:24:51 +0000

    cleaned up flexbox and added carousel

---
+++ micrositeprojectv2/assets/js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    $('.fade').slick({
+      dots: false,
+      infinite: true,
+      speed: 300,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 3000,
+      pauseonhover: true
+    });
+
+
+});
